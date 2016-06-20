1c1c261fb1c8a11252e80193e7847d9a019957f5
邓斌 <dengbin@dengbindeMacBook-Pro.local>
2016-06-20 23:11:41 +0800

    Initial Commit

---
+++ server/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  server
+//
+//  Created by 邓斌 on 6/20/16.
+//  Copyright © 2016 邓斌. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
