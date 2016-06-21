5d7b1ed276af36d774bff84b55075c2fac1fb7d7
b-deng <dengbin259@gmail.com>
2016-06-21 16:50:34 +0800

    http server

--- server/main.cpp
+++ server/main.cpp
@@ -1,16 +1,43 @@
 //
-//  main.cpp
-//  server
+// main.cpp
+// ~~~~~~~~
 //
-//  Created by 邓斌 on 6/20/16.
-//  Copyright © 2016 邓斌. All rights reserved.
+// Copyright (c) 2003-2015 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
 #include <iostream>
+#include <string>
+#include <boost/asio.hpp>
+#include "server.hpp"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    
-    return 0;
+int main(int argc, char* argv[])
+{
+  try
+  {
+    // Check command line arguments.
+    if (argc != 4)
+    {
+      std::cerr << "Usage: http_server <address> <port> <doc_root>\n";
+      std::cerr << "  For IPv4, try:\n";
+      std::cerr << "    receiver 0.0.0.0 80 .\n";
+      std::cerr << "  For IPv6, try:\n";
+      std::cerr << "    receiver 0::0 80 .\n";
+      return 1;
+    }
+
+    // Initialise the server.
+    http::server::server s(argv[1], argv[2], argv[3]);
+
+    // Run the server until stopped.
+    s.run();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "exception: " << e.what() << "\n";
+  }
+
+  return 0;
 }
